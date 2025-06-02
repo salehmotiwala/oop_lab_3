d0923c8dfbf7cfeae13303011958bd774da9c109
Saleh Motiwala <salehmotiwala@gmail.com>
2025-06-02 17:29:01 +0300

    URL to repo added

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,13 @@
 export const Footer = () => {
   return (
-    <div className="flex flex-col gap-0.5 text-center text-sm">
+    <div className="flex flex-col gap-0.5 text-center text-sm font-medium">
       <span>Created by: Muhammad Saleh</span>
 
-      <a href="" target="_blank">
+      <a
+        href="https://github.com/salehmotiwala/oop_lab_3"
+        target="_blank"
+        className="text-blue-500 underline"
+      >
         GitHub
       </a>
     </div>
